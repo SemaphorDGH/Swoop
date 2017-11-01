67a222e8ff110e52d0605f49f93923f8052f7f5d
Derek Wang <whc_derek@126.com>
2017-11-01 02:06:26 -0400

    faasf

--- app/src/main/java/semaphor/swoop/activities/MainActivity.java
+++ app/src/main/java/semaphor/swoop/activities/MainActivity.java
@@ -18,11 +18,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
 
         Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
-        finish();
     }
 
     @Override
