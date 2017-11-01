f86e2e6938e9670046ea9391b840150babadff3c
Derek Wang <whc_derek@126.com>
2017-11-01 01:15:14 -0400

    frontend changes

--- app/src/main/java/semaphor/swoop/activities/MainActivity.java
+++ app/src/main/java/semaphor/swoop/activities/MainActivity.java
@@ -22,7 +22,6 @@ public class MainActivity extends AppCompatActivity {
 
 
         Intent intent = new Intent(this, LoginActivity.class);
-        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         startActivity(intent);
         finish();
     }
